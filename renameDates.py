607386ed5e71675eda37bf0a2daa0264d2bcd5b4
刘硕 <liushuo@liushuodeMacBook-Air.local>
2020-12-24 13:53:43 +0800

    完成第六个项目——日期转换

--- renameDates.py
+++ renameDates.py
@@ -11,13 +11,28 @@ datePattern = re.compile(r'''^(.*?)
 ''', re.VERBOSE)
 
 # TODO: Loop over the files in the working directory
+for amerFilename in os.listdir():
+    mo = datePattern.search(amerFilename)
 
-# TODO: Skip files without a date
+    # TODO: Skip files without a date
+    if mo == None:
+        continue
 
-# TODO: Get the different parts of the filename
+    # TODO: Get the different parts of the filename
+    beforeFile = mo.group(1)
+    monthFile = mo.group(2)
+    dayFile = mo.group(4)
+    yearFile = mo.group(5)
+    afterFile = mo.group(7)
 
-# TODO: From the Chinese-style filename
+    # TODO: From the Chinese-style filename
+    chineseFilename = beforeFile + yearFile + '-' + monthFile + '-' + dayFile + afterFile
 
-# TODO: Get the full, absolute files paths
+    # TODO: Get the full, absolute files paths
+    absWorkDir = os.path.abspath('.')
+    amerFilename = os.path.join(absWorkDir, amerFilename)
+    chineseFilename = os.path.join(absWorkDir, chineseFilename)
 
-# TODO: Rename the files
+    # TODO: Rename the files
+    print('Renaming %s to %s...' % (amerFilename, chineseFilename))
+    shutil.move(amerFilename, chineseFilename)
